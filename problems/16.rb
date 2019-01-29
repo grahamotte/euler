f5f28bd65c056f3c497dc573662a204d69ab382b
Graham Otte <graham.otte@appfolio.com>
2019-01-28 20:43:42 -0800

    16

---
+++ problems/16.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/helper'
+
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 2^1000?
+
+def simple
+  (2 ** 1000).to_s.split('').inject(0) { |s, i| s += i.to_i }
+end
+
+run(16) do
+  simple
+end
